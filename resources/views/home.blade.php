835f9a5978d4c0bd601cd758a8c1eb4bb8539061
markcaf <marcocafassog@gmail.com>
2022-09-09 00:51:01 +0200

    test(home): print list of movies in the database

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,13 @@
 @section('title', 'Home')
 
 @section('main-content')
-
+    <div class="py-4">
+        <ul>
+            @foreach ($movies as $movie)
+                <li>
+                    {{ $movie->id }} - {{ $movie->title }}
+                </li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
