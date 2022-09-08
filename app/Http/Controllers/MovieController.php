0b22062b5f39b7d21e0040502d34543515186509
markcaf <marcocafassog@gmail.com>
2022-09-09 00:29:36 +0200

    refactor: change file name in movie controller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -5,12 +5,11 @@ namespace App\Http\Controllers;
 use App\Movie;
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class MovieController extends Controller
 {
     //
     public function index(){
         $movies = Movie::all();
-        dd($movies);
         return view('home', compact('movies'));
     }
 }
