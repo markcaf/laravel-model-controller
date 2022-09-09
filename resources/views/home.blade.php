15f1de4b08275677b950a90b741cad0ade5e7ee6
markcaf <marcocafassog@gmail.com>
2022-09-09 02:15:33 +0200

    feat: add the homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,27 +3,10 @@
 @section('title', 'Home')
 
 @section('main-content')
-    <div class="row p-5 justify-content-center">
-        @forelse ($movies as $movie)
-            <div class="card col-4 m-2" style="width: 18rem;">
-                {{-- <img src="..." class="card-img-top" alt="..."> --}}
-                <div class="card-body">
-                    <h5 class="card-title">{{ $movie->title }}</h5>
-                    <p class="card-text">Original title:<br> <em>{{ $movie->original_title }}</em></p>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Nationality: {{ $movie->nationality }}</li>
-                    <li class="list-group-item">Vote: {{ $movie->vote }}</li>
-                    <li class="list-group-item">Date: {{ $movie->date }}</li>
-                </ul>
-                <div class="card-body">
-                    <a href="#" class="card-link">More details</a>
-                </div>
-            </div>
-        @empty
-            <div>
-                <h2>There are no movies available. Try later.</h2>
-            </div>
-        @endforelse
+    <div class="text-center my-4">
+        <h1>Welcome in Laravel Movies</h1>
+        <p class="fs-5"><a href="{{ route('movies') }}">Click here</a> to start browsing</p>
     </div>
+
+    <img src="https://www.itl.cat/pngfile/big/46-465731_streaming-movies.jpg" alt="" class="img-fluid">
 @endsection
